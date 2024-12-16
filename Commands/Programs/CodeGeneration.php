cea9b9ed1856cccf0c1be5c5103a3c2e6e497c2f
eiji <ri2005hoshi@gmail.com>
2024-12-17 03:36:40 +0900

    CodeGeneration, Migrateの追加実装 - AbstractCommandクラスを継承 - getArguments関数を追加（未実装） - execute関数を追加（未実装） - MigrateクラスとCodeGenerationクラスをコマンドレジストリに追加

---
+++ Commands/Programs/CodeGeneration.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Commands\Programs;
+
+use Commands\AbstractCommand;
+
+class CodeGeneration extends AbstractCommand
+{
+
+    public static function getArguments(): array
+    {
+        // TODO: Implement getArguments() method.
+    }
+
+    public function execute(): int
+    {
+        // TODO: Implement execute() method.
+    }
+}
---
+++ Commands/Programs/Migrate.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Commands\Programs;
+
+use Commands\AbstractCommand;
+
+class Migrate extends AbstractCommand
+{
+
+    public static function getArguments(): array
+    {
+        // TODO: Implement getArguments() method.
+    }
+
+    public function execute(): int
+    {
+        // TODO: Implement execute() method.
+    }
+}
