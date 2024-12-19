a7042a0a7c70b15051a1007d531b551bd650acd2
eiji <ri2005hoshi@gmail.com>
2024-12-19 21:17:36 +0900

    HTMLRendererクラスの更新 - getFieldsメソッドを実装し、Content-Typeヘッダーを返す   - 'Content-Type' => 'text/html; charset=utf-8'を設定 - getContentメソッドを更新し、ビューファイルが存在しない場合に例外をスロー   - ビューファイルの存在をチェックし、存在しない場合は例外をスロー   - ビューファイルの内容を抽出し、ヘッダーとフッターを含めてレンダリング

--- Response/Render/HTMLRenderer.php
+++ Response/Render/HTMLRenderer.php
@@ -6,6 +6,14 @@ use Response\HTTPRenderer;
 
 class HTMLRenderer implements HTTPRenderer
 {
+    private string $viewFile;
+    private array $data;
+
+    public function __construct(string $viewFile, array $data)
+    {
+        $this->viewFile = $viewFile;
+        $this->data = $data;
+    }
     public function getFields(): array
     {
         return [
@@ -16,10 +24,32 @@ class HTMLRenderer implements HTTPRenderer
     public function getContent(): string
     {
         $viewPath = $this->getViewPath($this->viewFile);
+        if(!file_exists($viewPath)){
+            throw new \Exception("View file not found: " . $viewPath);
+        }
+
+        ob_get_clean();
+        extract($this->data);
+        require $viewPath;
+        require $this->getHeader() . ob_get_clean() . $this->getFooter();
     }
 
     private function getViewPath($viewFile)
     {
         return sprintf("%s/%s/Views/%s.php", __DIR__, "../../", $viewFile);
+    }
+
+    private function getHeader(): string
+    {
+        ob_start();
+        require $this->getViewPath("layout/header");
+        return ob_get_clean();
+    }
+
+    private function getFooter(): string
+    {
+        ob_start();
+        require $this->getViewPath("layout/footer");
+        return ob_get_clean();
     }
 }
