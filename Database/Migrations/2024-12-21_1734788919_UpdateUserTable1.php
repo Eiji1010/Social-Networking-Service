60b9296b6cd42705405fc5d420687a0a93a3968d
eiji <ri2005hoshi@gmail.com>
2024-12-21 23:31:39 +0900

    ユーザーテーブルにhandleカラムを追加

--- Database/Migrations/2024-12-21_1734788919_UpdateUserTable1.php
+++ Database/Migrations/2024-12-21_1734788919_UpdateUserTable1.php
@@ -7,6 +7,7 @@ class UpdateUserTable1 implements SchemaMigration
     public function up(): array
     {
         return [
+            "ALTER TABLE users ADD COLUMN handle VARCHAR(30)",
             "ALTER TABLE users ADD COLUMN age INT",
             "ALTER TABLE users ADD COLUMN place VARCHAR(255)",
             "ALTER TABLE users ADD COLUMN biography TEXT",
@@ -16,6 +17,7 @@ class UpdateUserTable1 implements SchemaMigration
     public function down(): array
     {
         return[
+            "ALTER TABLE users DROP COLUMN handle",
             "ALTER TABLE users DROP COLUMN age",
             "ALTER TABLE users DROP COLUMN place",
             "ALTER TABLE users DROP COLUMN biography",
