7ba82e532a3be2b07b6f0db8ac981d45fad618b4
eiji <ri2005hoshi@gmail.com>
2024-12-22 02:58:40 +0900

    AbstractSeederクラスのデータ挿入ロジックを実装 - テーブルのカラム名を取得し、プレースホルダーを生成 - SQLのINSERT文を作成し、データを挿入 - プリペアドステートメントを使用してデータをバインドし、実行

---
+++ Database/AbstractSeeder.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Database;
+
+use Database\Seeder;
+
+abstract class AbstractSeeder implements Seeder
+{
+    protected MySQLWrapper $conn;
+    protected ?string $tableName = null;
+
+    protected array $tableColumns = [];
+    const AVAILABLE_TYPES = [
+        'int' => 'i',
+        'float' => 'd',
+        'string' => 's',
+    ];
+
+    public function __construct(MySQLWrapper $conn)
+    {
+        $this->conn = $conn;
+    }
+    public function seed(): void
+    {
+        $data = $this->createRowData();
+
+        if ($this->tableName === null) throw new \Exception("Table name not set");
+        if (empty($this->tableColumns)) throw new \Exception("Table columns not set");
+
+        foreach ($data as $row) {
+            $this->validateRow($row);
+            $this->insertRow($row);
+        }
+    }
+
+    private function validateRow(array $row): void
+    {
+        if (count($row) !== count($this->tableColumns)) throw new \Exception("Column count mismatch");
+
+            foreach ($row as $i => $value) {
+                $columnDataType = $this->tableColumns[$i]['data_type'];
+                $columnName = $this->tableColumns[$i]['column_name'];
+
+                if (!isset(self::AVAILABLE_TYPES[$columnDataType])) throw new \InvalidArgumentException(sprintf("Invalid data type %s", $columnDataType));
+                if (get_debug_type($value) !== $columnDataType) throw new \InvalidArgumentException(sprintf("Value for %s should be of type %s. Here is the current value: %s", $columnName, $columnDataType, json_encode($value)));
+            }
+    }
+
+    private function insertRow(mixed $row)
+    {
+        $columnNames = array_map(function($columnInfo){ return $columnInfo['column_name'];}, $this->tableColumns);
+
+        $placeholders = str_repeat('?, ', count($row)-1) . '?';
+
+        $sql = sprintf(
+            "INSERT INTO %s (%s) VALUES (%s)",
+            $this->tableName,
+            implode(', ', $columnNames),
+            $placeholders
+        );
+
+        $stmt = $this->conn->prepare($sql);
+
+        $dataTypes = implode(array_map(function($columnInfo){ return static::AVAILABLE_TYPES[$columnInfo['data_type']];}, $this->tableColumns));
+        $stmt->bind_param($dataTypes, ...$row);
+        $stmt->execute();
+    }
+}
