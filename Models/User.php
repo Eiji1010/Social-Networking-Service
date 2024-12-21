ce966b9c4cf10d6f21f68046df53607674b0e1d6
eiji <ri2005hoshi@gmail.com>
2024-12-22 01:51:05 +0900

    Userクラスの更新 - コンストラクタにhandleとbiographyプロパティを追加   - handle: ユーザーのハンドルネームを表す文字列   - biography: ユーザーのバイオグラフィーを表す文字列

--- Models/User.php
+++ Models/User.php
@@ -16,6 +16,8 @@ class User implements Model
         private ?DateTimeStamp $timeStamp = null,
         private ?string $place = null,
         private ?int $age = null,
+        private ?string $handle = null,
+        private ?string $biography = null,
     ){}
 
     public function getId(): ?int {
@@ -48,5 +50,39 @@ class User implements Model
 
     public function setTimeStamp(?DateTimeStamp $timeStamp): void {
         $this->timeStamp = $timeStamp;
+    }
+
+    public function getAge(): ?int {
+        return $this->age;
+    }
+
+    public function setAge(?int $age): void {
+        $this->age = $age;
+    }
+
+    public function getHandle(): ?string{
+        return $this->handle;
+    }
+
+    public function setHandle(?string $handle): void{
+        $this->handle = $handle;
+    }
+
+    public function getBiography(): ?string{
+        return $this->biography;
+    }
+
+    public function setBiography(?string $biography): void{
+        $this->biography = $biography;
+    }
+
+    public function getPlace(): ?string
+    {
+        return $this->place;
+    }
+
+    public function setPlace(?string $place): void
+    {
+        $this->place = $place;
     }
 }
