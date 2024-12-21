1ba60eb050703089b04a398cd4b5efb37106b84f
eiji <ri2005hoshi@gmail.com>
2024-12-21 17:52:25 +0900

    フッターのHTMLコードをコメントアウト

--- Views/layout/footer.php
+++ Views/layout/footer.php
@@ -1,9 +1,9 @@
     </main>
-    <footer class="bg-light text-center text-lg-start">
-        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-            © 2024:
-            <a class="text-dark" href="/">SNS.com</a>
-        </div>
-    </footer>
+<!--    <footer class="bg-light text-center text-lg-start">-->
+<!--        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">-->
+<!--            © 2024:-->
+<!--            <a class="text-dark" href="/">SNS.com</a>-->
+<!--        </div>-->
+<!--    </footer>-->
 </body>
 </html>
