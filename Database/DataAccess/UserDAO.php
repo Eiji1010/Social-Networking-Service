5f5bb180d6abee27344c8718d84faa188efa1242
eiji <ri2005hoshi@gmail.com>
2024-12-22 01:00:26 +0900

    UserDAOインターフェースの更新 - updateProfileメソッドを追加し、ユーザープロフィールの更新を定義   - ユーザー名、ハンドル、年齢、居住地、バイオグラフィーを更新するためのメソッド

--- Database/DataAccess/UserDAO.php
+++ Database/DataAccess/UserDAO.php
@@ -9,4 +9,5 @@ interface UserDAO
     public function getById(int $id): ?User;
     public function getByEmail(string $email): ?User;
     public function getHashedPasswordById(int $id): ?string;
+    public function updateProfile(array $data): bool;
 }
