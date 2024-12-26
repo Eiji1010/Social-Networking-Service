4e305691d389509934b63d02905760592cc03d53
eiji <ri2005hoshi@gmail.com>
2024-12-27 00:57:19 +0900

    MessageDAOインターフェースの追加 - createメソッドを定義し、新しいメッセージを作成 - getByIdメソッドを定義し、指定されたIDのメッセージを取得 - getBySenderIdメソッドを定義し、指定された送信者IDのメッセージを取得

---
+++ Database/DataAccess/MessageDAO.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Database\DataAccess;
+
+use Models\Message;
+
+interface MessageDAO
+{
+    public function create(Message $message): bool;
+    public function getById(int $id): ?Message;
+    public function getBySenderId(int $senderId): ?array;
+}
