aa4c4fcb4c7d5d6cd4bce0868c922fd4862bb96d
eiji <ri2005hoshi@gmail.com>
2024-12-27 00:58:00 +0900

    DAOFactoryクラスの更新 - getMessageDAOメソッドを追加し、MessageDAOインターフェースの実装を返す   - MessageDAOImplクラスのインスタンスを返す

--- Database/DAOFactory.php
+++ Database/DAOFactory.php
@@ -2,7 +2,9 @@
 
 namespace Database;
 
+use Database\DataAccess\MessageDAO;
 use Database\DataAccess\UserDAO;
+use Database\Implementations\MessageDAOImpl;
 use Database\Implementations\UserDAOImpl;
 
 class DAOFactory
@@ -10,5 +12,10 @@ class DAOFactory
     public static function getUserDAO(): UserDAO
     {
         return new UserDAOImpl();
+    }
+
+    public static function getMessageDAO(): MessageDAO
+    {
+        return new MessageDAOImpl();
     }
 }
