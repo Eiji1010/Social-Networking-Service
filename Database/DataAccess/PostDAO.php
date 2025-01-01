2ca99ce76478d4b8bda5f49831dd2b5474444dae
eiji <ri2005hoshi@gmail.com>
2025-01-01 15:46:32 +0900

    PostDAOインターフェースにgetAllメソッドを追加し、countByUserIdメソッドの型宣言を修正

--- Database/DataAccess/PostDAO.php
+++ Database/DataAccess/PostDAO.php
@@ -9,6 +9,6 @@ interface PostDAO
     public function create(Post $post): bool;
     public function getById(int $id): ?Post;
     public function getByUserId(int $userId): ?array;
-    public function countByUserId($userId): int;
-    
+    public function countByUserId(int $userId): int;
+    public function getAll():array;
 }
