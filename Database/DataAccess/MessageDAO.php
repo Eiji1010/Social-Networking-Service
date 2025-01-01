a621ec1b95721d180ec6e433dfb469bdc2e32770
eiji <ri2005hoshi@gmail.com>
2025-01-01 11:00:19 +0900

    MessageDAOインターフェースにsenderIdによるメッセージ数をカウントするメソッドを追加

--- Database/DataAccess/MessageDAO.php
+++ Database/DataAccess/MessageDAO.php
@@ -9,4 +9,5 @@ interface MessageDAO
     public function create(Message $message): bool;
     public function getById(int $id): ?Message;
     public function getBySenderId(int $senderId): ?array;
+    public function countBySenderId($userId): int;
 }
